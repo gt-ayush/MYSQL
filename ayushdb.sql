cba1b760dfe8904ed34d5ec491c3e648972e6339
Ayush Kumar Gupta <gt-ayush@outlook.com>
2025-09-07 21:38:08 +0530

    added the database in sql

---
+++ ayushdb.sql
@@ -0,0 +1,90 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 07, 2025 at 05:58 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ayushdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emp`
+--
+
+CREATE TABLE `emp` (
+  `ID` int(11) NOT NULL,
+  `FN` varchar(255) DEFAULT NULL,
+  `LN` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emp1`
+--
+
+CREATE TABLE `emp1` (
+  `ID` int(11) NOT NULL,
+  `FN` varchar(255) DEFAULT NULL,
+  `LN` varchar(255) DEFAULT NULL,
+  `ED` varchar(255) DEFAULT 'oprations'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `emp1`
+--
+
+INSERT INTO `emp1` (`ID`, `FN`, `LN`, `ED`) VALUES
+(1, 'Ayush', 'Gupta', 'oprations'),
+(2, 'Moli', 'Gupta', 'oprations');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emp2`
+--
+
+CREATE TABLE `emp2` (
+  `ID` int(11) NOT NULL,
+  `FN` varchar(255) DEFAULT NULL,
+  `LN` varchar(255) DEFAULT NULL,
+  `Age` int(11) DEFAULT NULL
+) ;
+
+--
+-- Dumping data for table `emp2`
+--
+
+INSERT INTO `emp2` (`ID`, `FN`, `LN`, `Age`) VALUES
+(1, 'Ayush', 'Gupta', 22);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `emp`
+--
+ALTER TABLE `emp`
+  ADD UNIQUE KEY `ID` (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
